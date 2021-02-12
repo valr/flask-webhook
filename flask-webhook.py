08e0b9343819776b56868eb164437283a8b73479
Valère Monseur <valere.monseur@ymail.com>
2021-02-12 22:03:19 +0100

    rework the webhook to store the event payload in a defined directory

--- flask-webhook.py
+++ flask-webhook.py
@@ -1,8 +1,9 @@
 import hashlib
 import hmac
+import json
 import os
 
-from flask import Flask, request
+from flask import Flask, jsonify, request
 
 instance_path = os.environ.get('INSTANCE_PATH')
 if instance_path:
@@ -18,29 +19,29 @@ application.config.from_pyfile('flask-webhook.conf')
 # https://docs.github.com/en/developers/webhooks-and-events/webhooks
 @application.route('/github', methods=['POST'])
 def webhook_github():
+    secret_key = application.config.get('GITHUB_SECRET_KEY')
+    event_path = application.config.get('GITHUB_EVENT_PATH')
+
+    if not secret_key or not event_path:
+        return jsonify(
+            error='Incorrect Github webhook configuration on server'), 503
+
     # get and verify the signature to ensure the payload is valid
     signature = request.headers.get('X-Hub-Signature-256')
     if not signature or not signature.startswith('sha256='):
-        return 'Missing or invalid X-Hub-Signature-256', 400
+        return jsonify(error='Missing or invalid X-Hub-Signature-256'), 400
 
-    key = bytes(application.config.get('GITHUB_SECRET_KEY', ''), 'utf-8')
-    if not key:
-        return 'Missing Github secret key on server', 503
-
-    digest = hmac.new(key, request.data, hashlib.sha256).hexdigest()
+    digest = hmac.new(bytes(secret_key, 'utf-8'),
+                      request.data, hashlib.sha256).hexdigest()
     if not hmac.compare_digest(signature, 'sha256=' + digest):
-        return 'Invalid X-Hub-Signature-256', 400
+        return jsonify(error='Invalid X-Hub-Signature-256'), 400
 
-    # get the event and the payload
+    # get and store the event
     event = request.headers.get('X-GitHub-Event')
+    delivery = request.headers.get('X-GitHub-Delivery')
     payload = request.get_json(silent=True)
 
-    # print(event)
-    # print(payload)
+    with open(os.path.join(event_path, event + '.' + delivery), 'w') as file:
+        file.write(json.dumps(payload))
 
-    # process push event
-    if event == 'push':
-        repository = payload.get('repository', {}).get('name')
-        print(f'processing event: "{event}" on repository: "{repository}"')
-
-    return 'OK'
+    return jsonify({})
