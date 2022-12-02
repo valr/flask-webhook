74218c7cbc76f13ed71b09ad8672543400bf4948
Valère Monseur <dev@vale.re>
2022-12-02 11:15:33 +0100

    some reformatting

--- flask_webhook/__init__.py
+++ flask_webhook/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 
-def create_application(instance_path, config_file='flask-webhook.conf'):
+def create_application(instance_path, config_file="flask-webhook.conf"):
     if instance_path:
         application = Flask(
             __name__, instance_relative_config=True, instance_path=instance_path
@@ -12,6 +12,7 @@ def create_application(instance_path, config_file='flask-webhook.conf'):
     application.config.from_pyfile(config_file)
 
     from .github import blueprint as github_blueprint
+
     application.register_blueprint(github_blueprint, url_prefix="")
 
     return application
